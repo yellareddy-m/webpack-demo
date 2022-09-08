8f9960d305dc9b26c09df379c314eaf547e213e8
YellaReddy <yem@softwareag.com>
2022-09-08 17:27:56 +0530

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const { mode } = require("webpack-nano/argv");
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+
+module.exports = {
+    mode,
+    plugins: [
+        new MiniHtmlWebpackPlugin({ context: { title: "Demo" } }),
+    ],
+};
